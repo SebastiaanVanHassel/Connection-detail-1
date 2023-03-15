ae93e08e3f69ed1c2a14633c6c4ff4984e8b0a62
SebastiaanVanHassel <101708661+SebastiaanVanHassel@users.noreply.github.com>
2023-03-15 20:31:39 +0100

    Add files via upload

---
+++ beam2Ddrawer.py
@@ -0,0 +1,96 @@
+from math import sqrt
+import streamlit as st
+import matplotlib.pyplot as plt
+
+import Profiles
+
+#Profiles library
+all_attr_Profiles = dir(Profiles)
+Profile_names = ()
+for attribute in all_attr_Profiles:
+    obj = getattr(Profiles, attribute)
+    if isinstance(obj, Profiles.Profile):
+        Profile_names += (obj.name,)
+
+# -------------------------------------------------- #
+st. set_page_config(layout="wide")
+st.header("Beam 2D drawer")
+
+col1, col2 = st.columns(2)
+with col1:
+    sectiontype = st.radio("Section:",('Standard Rolled', 'Welded'))
+
+    col11, col12, col13, col14, col15 = st.columns(5)
+    if sectiontype == 'Standard Rolled':
+        beam_name = st.selectbox("",Profile_names, 16)
+        beam = getattr(Profiles, beam_name)
+        with col11:
+            st.write("Height \n [mm]: \n", beam.h)
+        with col12:
+            st.write("Width Top Flange  [mm]: ", beam.b)
+            st.write("Thickness Top Flange [mm]: ", beam.tf)
+        with col13:
+            st.write("Web thickness  [mm]: ", beam.tw)
+        with col14:
+            st.write("Width Bottom Flange  [mm]: ", beam.b)
+            st.write("Thickness Bottom Flange [mm]: ", beam.tf)
+        with col15:
+            st.write("Weld Radius [mm]: ", beam.r)
+    else:
+        with col11:
+            h = st.number_input("Height [mm]: ", 1, 3000, 400)
+        with col12:
+            btf = st.number_input("Width Top Flange  [mm]: ", 1, 800, 300)
+            ttf = st.number_input("Thickness Top Flange [mm]: ", 1, 100, 20)
+        with col13:
+            tw = st.number_input("Web thickness  [mm]: ", 1, 100, 10)
+        with col14:
+            bbf = st.number_input("Width Bottom Flange  [mm]: ", 1, 800, 300)
+            tbf = st.number_input("Thickness Bottom Flange [mm]: ", 1, 100, 20)
+        with col15:
+            r = st.number_input("Weld Radius [mm]: ", 1, 100, 10)
+    
+    L_beam = st.number_input("Length [m]: ", 1.00, 30.00, 8.00)
+
+with col2:
+    if sectiontype == 'Standard Rolled':
+        x_topflangecs = [-beam.b/2,  -beam.b/2,  beam.b/2,  beam.b/2, -beam.b/2]
+        y_topflangecs = [-beam.tf, 0, 0, -beam.tf, -beam.tf]
+        x_bottomflangecs = [-beam.b/2,  -beam.b/2,  beam.b/2,  beam.b/2, -beam.b/2]
+        y_bottomflangecs = [-beam.h, -beam.h+beam.tf, -beam.h+beam.tf, -beam.h, -beam.h]
+        x_webcs = [-beam.tw/2,  -beam.tw/2,  +beam.tw/2,  +beam.tw/2, -beam.tw/2]
+        y_webcs = [-beam.h+beam.tf, -beam.tf, -beam.tf, -beam.h+beam.tf, -beam.h+beam.tf]
+        # Create a new plot
+        fig, ax = plt.subplots()
+        # Plot the rectangle
+        ax.plot(x_topflangecs, y_topflangecs, color='blue')
+        ax.plot(x_bottomflangecs, y_bottomflangecs, color='blue')
+        ax.plot(x_webcs, y_webcs, color='lightblue')
+        # Set the x and y limits of the plot
+        ax.set_xlim([-beam.b, beam.b])
+        ax.set_ylim([-beam.h-15, 15])
+        st.pyplot(fig)
+    else:
+        st.write("welded section, image to be continued")
+    
+
+st.subheader("BricsCAD prompt generator!")
+def draw_rec(x_coords, y_coords):
+    assert len(x_coords) == len(y_coords), "Error: x and y coordinate arrays must have the same length"
+    n = len(x_coords)
+
+    commands = ["L"]  # start the line command
+    for i in range(n):
+        # add the current point to the list of commands
+        x, y = x_coords[i], y_coords[i]
+        commands.append(f"{x},{y}")
+
+    # return the list of commands as a single string separated by newlines
+    return "  \n  ".join(commands)
+
+topflange = draw_rec(x_topflangecs, y_topflangecs)
+st.write(topflange)
+bottomflange = draw_rec(x_bottomflangecs, y_bottomflangecs)
+st.write(bottomflange)
+web = draw_rec(x_webcs, y_webcs)
+st.write(web)
--- knuppel.py
+++ knuppel.py
@@ -4,8 +4,7 @@ import streamlit as st
 
 import Profiles
 
-st.title('Knüppel Anschluss')
-
+#Profiles library
 all_attr_Profiles = dir(Profiles)
 Profile_names = ()
 for attribute in all_attr_Profiles:
@@ -14,120 +13,165 @@ for attribute in all_attr_Profiles:
         Profile_names += (obj.name,)
 print(Profile_names)
 
-beam_name = st.selectbox("Profile Beam : ",Profile_names, 16)
-beam = getattr(Profiles, beam_name)
-beam.h = beam.h
-st.write("Profile  = ", beam.name)
-st.write("Height           h  [mm]  = ", beam.h)
-st.write("Width            b  [mm]  = ", beam.b)
-st.write("Web thickness    tw [mm]  = ", beam.tw)
-st.write("Flange thickness tf [mm]  = ", beam.tf)
-st.write("Sectional area   A  [cm2] = ", beam.A)
 
-L_beam = st.number_input("Length of the beam [m]: ", 1.00, 30.00, 8.00)
+# HEADER
+st. set_page_config(layout="wide")
+st.header('Knüppel Anschluss')
 
-F1 = st.number_input("Kraft F1: ", 1, 10000, 525)
-#F1 = 525    #kN
+col1, col2, col3 = st.columns(3)
 
-x = st.number_input("x [cm]: ", 1.00, 50.00, 12.25)
-y = st.number_input("y [cm]: ", 1.00, 100.00, 34.75)
-l = x + y
-alpha = x/l
-beta = y/l
+with col1:
+    st.subheader("BEAM")   # INPUT BEAM
+    # Profile
+    col1b, col1l = st.columns(2)
+    with col1b:
+        beam_name = st.selectbox("Profile: ",Profile_names, 16)
+    beam = getattr(Profiles, beam_name)
 
-F2 = F1/beta
-st.write("Kraft F2 = " , round(F2,0), " kN")
-F3 = F2*alpha
-st.write("Kraft F3 = " , round(F3,0), " kN")
+    # Length
+    with col1l:
+        L_beam = st.number_input("Length [m]: ", 1.00, 30.00, 8.00)
 
-M_max = F3*y    #kNcm
-V_max = F1      #kN
+    st.subheader("KNÜPPEL")   # INPUT KNUPPEL
+    F1 = st.number_input("Kraft F1: ", 1, 10000, 525)
+    col1x, col1y = st.columns(2)
+    with col1x:
+        x = st.number_input("x [cm]: ", 1.00, 50.00, 12.25)
+    with col1y:
+        y = st.number_input("y [cm]: ", 1.00, 100.00, 34.75)
+    l = x + y
+    alpha = x/l
+    beta = y/l
+    with col1x:
+        F2 = F1/beta;       st.write("Kraft F2 = " , round(F2,0), " kN")
+    with col1y:
+        F3 = F2*alpha;      st.write("Kraft F3 = " , round(F3,0), " kN")
+    M_max = F3*y    #kNcm
+    V_max = F1      #kN
+    #Knuppel dimensions
+    
+    col1li, col1re = st.columns(2)
+    with col1li:
+        n = st.radio("Anzahl : ",(1, 2, 3, 4), horizontal=True)
+        breite = st.slider("Breite [mm]: ", 10, 100, 50, 5)
+        aufL = st.slider("Auflager breite links [mm]: ", 40, 120, 80, 10)
+    with col1re:
+        e_k = st.number_input("e [cm] = ", 1, 20, 8)
+        hohe = st.slider("Höhe [mm]: ", 10, 300, 160, 5)
+        aufR = st.slider("Auflager breite rechts [mm]: ", 40, 120, 80, 10)
 
-#Knuppel dimensions
-n = st.selectbox("Anzahl : ",(1, 2, 3, 4))
-breite = st.slider("Breite [cm]: ", 10, 100, 50, 5)
-hohe = st.slider("Höhe [cm]: ", 10, 300, 160, 5)
-aufL = st.slider("Auflager breite links [cm]: ", 40, 120, 80, 10)
-aufR = st.slider("Auflager breite rechts [cm]: ", 40, 120, 80, 10)
-
-#--------------------------------------------#
+    
 #Nachweis Knuppel
-stahl_knuppel = st.selectbox("Stahl gute Knüppel : ",("S235", "S275", "S355", "S460"), 3)
-if (stahl_knuppel == "S460") and (breite > 40):
-    fy_knuppel = 41                 #kN/cm2
-else:
-    fy_knuppel = 44
-st.write("fy knuppel = ", fy_knuppel)
-W_pl =(n*breite*pow(hohe,2))/4/1000  #cm3
-sigma_pl = M_max/W_pl
-
-tau_pl = V_max/(n*breite*hohe)*100  #kN/cm2
-
-vonMises = sqrt(pow(sigma_pl,2) + 3*pow(tau_pl,2))
-
-UC_knuppel = vonMises/fy_knuppel
-st.write("Nachweis Knuppel = ", round(UC_knuppel,2))
-#--------------------------------------------#
+with col2:
+    st.subheader("NACHWEIS KNÜPPEL")
+    stahl_knuppel = st.selectbox("Stahl gute Knüppel : ",("S235", "S275", "S355", "S460"), 3)
+    if (stahl_knuppel == "S460") and (breite > 40):
+        fy_knuppel = 41                 #kN/cm2
+    else:
+        fy_knuppel = 44
+    st.write("fy knuppel = ", fy_knuppel)
+    W_pl =(n*breite*pow(hohe,2))/4/1000  #cm3
+    sigma_pl = M_max/W_pl
+    tau_pl = V_max/(n*breite*hohe)*100  #kN/cm2
+    vonMises = sqrt(pow(sigma_pl,2) + 3*pow(tau_pl,2))
+    UC_knuppel = vonMises/fy_knuppel
+    col2kl, col2kr = st.columns(2)
+    with col2kl:
+        st.write("Nachweis Knuppel")
+    with col2kr:
+        if UC_knuppel < 1.00:
+            st.success(str(round(UC_knuppel,2)) +'< 1.00')
+        else:
+            st.error(str(round(UC_knuppel,2)) +'> 1.00')
 
 #--------------------------------------------#
 #Welds
-fu_welds = 49       #kN/cm2
-yM2 = 1.25
-beta_w = 0.9
-fvw = fu_welds/(sqrt(3)*beta_w*1.25)
+    st.subheader("NACHWEIS WELDS")
+    fu_welds = 49       #kN/cm2
+    yM2 = 1.25
+    beta_w = 0.9
+    fvw = fu_welds/(sqrt(3)*beta_w*1.25)
 
-# --- #1 WELD --- #
-a1 = st.selectbox("a (Weld 1)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 4)
-Fw_Rd1 = fvw*a1/10       #kN/cm
-Lweld1 = breite/10      #cm
-Fweld_Rd1 = Fw_Rd1*Lweld1
+    # --- #1 WELD --- #
+    a1 = st.selectbox("a (Weld 1)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 4)
+    Fw_Rd1 = fvw*a1/10       #kN/cm
+    Lweld1 = breite/10      #cm
+    Fweld_Rd1 = Fw_Rd1*Lweld1
 
-# --- #2 WELD --- #
-n_welds2 = n*4
-a2 = st.selectbox("a (Weld 2)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 4)
-Fw2 = F2-Fweld_Rd1
-Fweld2 = Fw2/n_welds2
-Lweld2 = hohe               #mm
-Fw_Rd2 = Fweld2/Lweld2*10         #kN/cm
-Fweld_Rd2 = fvw*a2/10
-UC_weld2 = Fw_Rd2/Fweld_Rd2
-st.write("Nachweis weld 2 = ", round(UC_weld2,2))
+    # --- #2 WELD --- #
+    col2w2a, col2w2u = st.columns(2)
+    n_welds2 = n*4
+    with col2w2a:
+        a2 = st.selectbox("a (Weld 2)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 4)
+    Fw2 = F2-Fweld_Rd1
+    Fweld2 = Fw2/n_welds2
+    Lweld2 = hohe               #mm
+    Fw_Rd2 = Fweld2/Lweld2*10         #kN/cm
+    Fweld_Rd2 = fvw*a2/10
+    UC_weld2 = Fw_Rd2/Fweld_Rd2
+    with col2w2u:
+        st.write("Nachweis weld 2")
+        if UC_weld2 < 1.00:
+            st.success(str(round(UC_weld2,2)) + '  < 1.00')
+        else:
+            st.error(str(round(UC_weld2,2)) + '> 1.00')
 
-# --- #3 WELD --- #
-n_welds3 = n*2
-a3 = st.selectbox("a (Weld 3)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 4)
-Fw3 = F3                    #kN
-Fweld3 = Fw3/n_welds3       #kN
-Lweld3 = aufR               #mm
-Fw_Rd3 = Fweld3/Lweld3*10         #kN/cm
-Fweld_Rd3 = fvw*a3/10
-UC_weld3 = Fw_Rd3/Fweld_Rd3
-st.write("Nachweis weld 3 = ", round(UC_weld3,2))
+    # --- #3 WELD --- #
+    n_welds3 = n*2
+    with col2w2a:
+        a3 = st.selectbox("a (Weld 3)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 4)
+    Fw3 = F3                    #kN
+    Fweld3 = Fw3/n_welds3       #kN
+    Lweld3 = aufR               #mm
+    Fw_Rd3 = Fweld3/Lweld3*10         #kN/cm
+    Fweld_Rd3 = fvw*a3/10
+    UC_weld3 = Fw_Rd3/Fweld_Rd3
+    with col2w2u:
+        st.write("Nachweis weld 3")
+        if UC_weld3 < 1.00:
+            st.success(str(round(UC_weld3,2)) +'< 1.00')
+        else:
+            st.error(str(round(UC_weld3,2)) +'> 1.00')
 
-# --- #4 WELD --- #
-n_welds4 = 2
-a4 = st.selectbox("a (Weld 4)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 2)
-Fw4 = F2                    #kN
-Fweld4 = Fw4/n_welds4       #kN
-Lweld4 = 360                #mm
-Fw_Rd4 = Fweld4/Lweld4*10         #kN/cm
-Fweld_Rd4 = fvw*a4/10
-UC_weld4 = Fw_Rd4/Fweld_Rd4
-st.write("Nachweis weld 4 = ", round(UC_weld4,2))
+    # --- #4 WELD --- #
+    n_welds4 = 2
+    with col2w2a:
+        a4 = st.selectbox("a (Weld 4)  [mm] = ",(3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 2)
+    Fw4 = F2                    #kN
+    Fweld4 = Fw4/n_welds4       #kN
+    Lweld4 = 360                #mm
+    Fw_Rd4 = Fweld4/Lweld4*10         #kN/cm
+    Fweld_Rd4 = fvw*a4/10
+    UC_weld4 = Fw_Rd4/Fweld_Rd4
+    with col2w2u:
+        st.write("Nachweis weld 4")
+        if UC_weld4 < 1.00:
+            st.success(str(round(UC_weld4,2)) +'< 1.00')
+        else:
+            st.error(str(round(UC_weld4,2)) +'> 1.00')
 
-#Nachweis vertikales Blech
-fy_blech = 35.5     #kN/cm2
-t = st.slider("t [cm] (Dicke Blech): ", 1.0, 5.0, 2.0, 0.5)
-b = st.number_input("b [cm] (Breite Blech): ", 1.0, 100.0, 30.5)
-bnet = b - (n*breite)/10
-Fblech = F2 - Fweld_Rd1
-sigma_blech = Fblech/t/bnet     #kN/cm2
-UC_blech = sigma_blech/fy_blech
-st.write("Nachweis vertikales blech = ", round(UC_blech,2))
+    #Nachweis vertikales Blech
+    fy_blech = 35.5     #kN/cm2
+    with col2w2a:
+        t = st.slider("t [cm] (Dicke Blech): ", 1.0, 5.0, 2.0, 0.5)
+    b = beam.b
+    with col2w2a:
+        st.write("Breite Blech is Breite Träger =  ", beam.b)
+    bnet = b - (n*breite)/10
+    Fblech = F2 - Fweld_Rd1
+    sigma_blech = Fblech/t/bnet     #kN/cm2
+    UC_blech = sigma_blech/fy_blech
+    with col2w2u:
+        st.write("Nachweis vertikales blech", )
+        if UC_blech < 1.00:
+            st.success(str(round(UC_blech,2)) + '< 1.00')
+        else:
+            st.error(str(round(UC_blech,2)) + '> 1.00')
 
 
-
+# Knuppelanschluss and Beam visualisation
 import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
 
 #KNUPPEL DRAWING
 # Define the coordinates in cm!
@@ -146,21 +190,81 @@ y_topflange = [-0.5-beam.tf/10, -0.5, -0.5, -0.5-beam.tf/10, -0.5-beam.tf/10]
 x_bottomflange = [aufL/20+x+t/2, aufL/20+x+t/2, aufL/20+x+t/2+L_beam*100, aufL/20+x+t/2+L_beam*100, aufL/20+x+t/2]
 y_bottomflange = [-0.5-beam.h/10, -0.5-beam.h/10+beam.tf/10, -0.5-beam.h/10+beam.tf/10, -0.5-beam.h/10, -0.5-beam.h/10]
 
+#DIMENSIONS DRAWING
+arrowx = FancyArrowPatch((aufL/20, hohe/10+5), (aufL/20+x, hohe/10+5), arrowstyle='<->', mutation_scale=10, shrinkA=0, shrinkB=0)
+arrowy = FancyArrowPatch((aufL/20+x, hohe/10+5), (aufL/20+x+y, hohe/10+5), arrowstyle='<->', mutation_scale=10, shrinkA=0, shrinkB=0)
+
+#CROSS_SECTION DRAWING
+Lcs = beam.b/20 +5
+#Knuppels
+if n == 1:
+    xknuppelcs = [-Lcs-breite/20,  -Lcs-breite/20,  -Lcs+breite/20,  -Lcs+breite/20, -Lcs-breite/20]
+    yknuppelcs = [           0,  hohe/10,  hohe/10,             0,             0]
+elif n == 2:
+    xknuppelcs = [-Lcs-breite/20-e_k/2,  -Lcs-breite/20-e_k/2,  -Lcs+breite/20-e_k/2,  -Lcs+breite/20-e_k/2, -Lcs-breite/20+e_k/2, -Lcs-breite/20+e_k/2, -Lcs+breite/20+e_k/2, -Lcs+breite/20+e_k/2]
+    yknuppelcs = [           0,  hohe/10,  hohe/10,             0,             0,  hohe/10, hohe/10, 0 ]
+#Beam
+x_topflangecs = [-Lcs-beam.b/20,  -Lcs-beam.b/20,  -Lcs+beam.b/20,  -Lcs+beam.b/20, -Lcs-beam.b/20]
+y_topflangecs = [-0.5-beam.tf/10, -0.5, -0.5, -0.5-beam.tf/10, -0.5-beam.tf/10]
+x_bottomflangecs = [-Lcs-beam.b/20,  -Lcs-beam.b/20,  -Lcs+beam.b/20,  -Lcs+beam.b/20, -Lcs-beam.b/20]
+y_bottomflangecs = [-0.5-beam.h/10, -0.5-beam.h/10+beam.tf/10, -0.5-beam.h/10+beam.tf/10, -0.5-beam.h/10, -0.5-beam.h/10]
+x_webcs = [-Lcs-beam.tw/20,  -Lcs-beam.tw/20,  -Lcs+beam.tw/20,  -Lcs+beam.tw/20, -Lcs-beam.tw/20]
+y_webcs = [-0.5-beam.h/10+beam.tf/10, -0.5-beam.tf/10, -0.5-beam.tf/10, -0.5-beam.h/10+beam.tf/10, -0.5-beam.h/10+beam.tf/10]
+
 
 # Create a new plot
 fig, ax = plt.subplots()
 
 # Plot the rectangle
-ax.plot(xknuppel, yknuppel)
-ax.plot(xblech, yblech)
-ax.plot(xaufL, yaufL)
-ax.plot(xaufR, yaufR)
-ax.plot(x_topflange, y_topflange)
-ax.plot(x_bottomflange, y_bottomflange)
+ax.plot(x_topflange, y_topflange, color='blue')
+ax.plot(x_bottomflange, y_bottomflange, color='blue')
+ax.plot(xknuppel, yknuppel, color='red')
+ax.plot(xblech, yblech, color='orange')
+ax.plot(xaufL, yaufL, color='red')
+ax.plot(xaufR, yaufR, color='red')
+ax.add_patch(arrowx)
+ax.add_patch(arrowy)
+plt.text(aufL/20+x/2, hohe/10+6, "x")
+plt.text(aufL/20+x+y/2, hohe/10+6, "y")
+ax.plot(xknuppelcs, yknuppelcs, color='red')
+ax.plot(x_topflangecs, y_topflangecs, color='blue')
+ax.plot(x_bottomflangecs, y_bottomflangecs, color='blue')
+ax.plot(x_webcs, y_webcs, color='blue')
 
 # Set the x and y limits of the plot
-ax.set_xlim([-x/10-10, 100])
+ax.set_xlim([-x/10-2*beam.b/10-10, 100])
 ax.set_ylim([-beam.h/10-10, hohe/10+10])
 
-with st.sidebar:
+with col3:
     st.pyplot(fig)
+
+    col31, col32 = st.columns(2)
+    with col31:
+        st.write("KNÜPPEL:")
+        st.write("Breite = ", n, " x ", breite, "mm")
+        st.write("Höhe = ", hohe, "mm")
+        st.write("x = ", x, "cm")
+        st.write("y = ", y, "cm")
+        st.write("WELDS:")
+        st.write("a1 = ", a1, "mm")
+        st.write("a2 = ", a2, "mm")
+        st.write("a3 = ", a3, "mm")
+        st.write("a4 = ", a4, "mm")
+    
+    with col32:
+        st.write("Kraft F1 = ", F1, "kN")
+        st.write("Max. Moment = ", round(M_max,0), "kNcm")
+        st.write("Max. Shear Force = ", V_max, "kN")
+
+        st. write("BEAM:")
+        st.write(beam.name)
+        st.write("Height           h  [mm]  = ", beam.h)
+        st.write("Width            b  [mm]  = ", beam.b)
+        st.write("Web thickness    tw [mm]  = ", beam.tw)
+        st.write("Flange thickness tf [mm]  = ", beam.tf)
+        st.write("Sectional area   A  [cm2] = ", beam.A)
+
+
+#convert the drawing into 2D drawing propmt commands for BricsCAD
+#xknuppel
+#yknuppel
